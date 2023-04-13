ed5af837c3d4f8891f9243fe0f1e93b67360b196
kawamou <kwmkzsd@gmail.com>
2023-04-13 12:21:01 +0900

    signin -> auth

--- toy-nft-marketplace/src/app/auth/page.tsx
+++ toy-nft-marketplace/src/app/auth/page.tsx
@@ -1,29 +1,30 @@
 "use client";
+import { AuthContext } from "@/context/authContext";
 import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const Signin = () => {
+  const { auth } = useContext(AuthContext);
   const [initAuth, isLogin, login, logout, getSigner, getAddress, getBalance] =
-    useAuth();
+    auth!;
   const router = useRouter();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="flex justify-center items-center h-screen w-full gap-4">
       <button
         className=" bg-pink-500 text-white rounded px-4 py-2 shadow-lg"
         onClick={async () => {
-          console.log("login");
           const isLoggedIn = await isLogin();
+          console.log(isLoggedIn);
           if (!isLoggedIn) {
-            setIsLoggedIn(true);
+            console.log("login");
             const web3auth = await initAuth();
             const provider = await web3auth?.connect();
             if (!provider) return;
-            await login(provider);
-            router.push("/");
+            login(provider);
           }
+          router.push("/");
         }}
       >
         Login
@@ -33,7 +34,6 @@ const Signin = () => {
         onClick={async () => {
           console.log("logout");
           await logout();
-          setIsLoggedIn(false);
           router.push("/");
         }}
       >
