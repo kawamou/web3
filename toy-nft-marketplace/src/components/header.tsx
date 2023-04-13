78205efa1f81ad71b2d66b8ce12e84ea1f597f47
kawamou <kwmkzsd@gmail.com>
2023-04-13 10:07:22 +0900

    Headerを実装

---
+++ toy-nft-marketplace/src/components/header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <nav className="border-b p-6">
+      <p className="text-4xl font-bold">Marketplace</p>
+      <div className="flex mt-4">
+        <Link href="/" className="mr-4 text-pink-500">
+          Home
+        </Link>
+        <Link href="/create-nft" className="mr-6 text-pink-500">
+          Sell NFT
+        </Link>
+        <Link href="/my-nfts" className="mr-6 text-pink-500">
+          My NFTs
+        </Link>
+        <Link href="/dashboard" className="mr-6 text-pink-500">
+          Dashboard
+        </Link>
+        <Link href="/signin" className="mr-6 text-pink-500">
+          Signin
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
