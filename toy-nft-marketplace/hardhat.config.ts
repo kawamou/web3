8a2a353de4691c1ddaba7ec968cf21b1b6ee2a86
kawamou <kwmkzsd@gmail.com>
2023-04-16 22:14:58 +0900

    hardhat.config.tsを修正

--- toy-nft-marketplace/hardhat.config.ts
+++ toy-nft-marketplace/hardhat.config.ts
@@ -6,12 +6,8 @@ const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337,
+      chainId: 80001,
     },
-    // polygon_mumbai: {
-    //   url: "https://rpc-mumbai.maticvigil.com",
-    //   accounts: [process.env.PRIVATE_KEY ?? ""],
-    // },
   },
   solidity: {
     version: "0.8.18",
