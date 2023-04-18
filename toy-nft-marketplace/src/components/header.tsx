3ec31c92b8e95de6a9e9e15f883c7c488774d32b
kawamou <kwmkzsd@gmail.com>
2023-04-19 00:27:54 +0900

    Headerの404部分を除去しカラーも変更

--- toy-nft-marketplace/src/components/header.tsx
+++ toy-nft-marketplace/src/components/header.tsx
@@ -2,22 +2,22 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <nav className="border-b p-6">
+    <nav className="border-b border-gray-600 p-6">
       <p className="text-4xl font-bold">Marketplace</p>
       <div className="flex mt-4">
-        <Link href="/" className="mr-4 text-pink-500">
+        <Link href="/" className="mr-4 text-blue-500">
           Home
         </Link>
-        <Link href="/create_nft" className="mr-6 text-pink-500">
+        <Link href="/create_nft" className="mr-6 text-blue-500">
           Sell NFT
         </Link>
-        <Link href="/my_nfts" className="mr-6 text-pink-500">
+        <Link href="/" className="mr-6 text-blue-500">
           My NFTs
         </Link>
-        <Link href="/dashboard" className="mr-6 text-pink-500">
+        <Link href="/" className="mr-6 text-blue-500">
           Dashboard
         </Link>
-        <Link href="/auth" className="mr-6 text-pink-500">
+        <Link href="/auth" className="mr-6 text-blue-500">
           Login / Logout
         </Link>
       </div>
